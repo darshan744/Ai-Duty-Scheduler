8c08447db73961a613c8f7479fe071b1712ffc02
darshan744 <darshankumaravelk@gmail.com>
2025-08-01 16:00:43 +0530

    Added Components using shadcn

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,12 @@
 import "./App.css";
 //import ThemeButton from "./components/ui/ThemeButton";
 import { ThemeProvider } from "./Context/ThemeContext/ThemeProvider";
-
+import Auth from "./pages/Auth";
 function App() {
   return (
   <ThemeProvider >
-    <div>
-      
+    <div className="flex flex-col min-h-screen justify-center items-center">
+     <Auth /> 
     </div>
   </ThemeProvider>
   )
--- frontend/src/pages/Auth.tsx
+++ frontend/src/pages/Auth.tsx
@@ -1,13 +1,17 @@
+import { LoginForm } from "./login-form"
 
 
 function Auth() {
-    return(
-    <>
-    <div className="">
-
+    return (
+        <>
+    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm">
+        <LoginForm />
+      </div>
     </div>
-    </>
-     )
+        
+        </>
+    )
 }
 
 export default Auth
