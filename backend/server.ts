f9480f5114705d224c28ca5d6786bd4c44c3cdca
darshan744 <darshankumaravelk@gmail.com>
2025-08-01 10:45:02 +0530

    Setup: Added Tailwindcss , Shadcnui

--- backend/server.ts
+++ backend/server.ts
@@ -1,1 +1,13 @@
-console.log("HELLO");
+import app from "./app";
+
+import environments from "./environments";
+
+const port = environments.PORT;
+
+if (!port) {
+  console.log("Port is not defined ");
+  process.exit(1);
+}
+app.listen(environments.PORT, () => {
+  console.log(`Server running in ${port}`);
+});
